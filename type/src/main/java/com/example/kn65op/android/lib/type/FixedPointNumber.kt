251582b655251ee7a9101ae1d0fedc5881b600f8
kn65op <199537+kn65op@users.noreply.github.com>
2020-05-10 16:24:55 +0200

    Uninitialized is zero

--- type/src/main/java/com/example/kn65op/android/lib/type/FixedPointNumber.kt
+++ type/src/main/java/com/example/kn65op/android/lib/type/FixedPointNumber.kt
@@ -1,4 +1,7 @@
 package com.example.kn65op.android.lib.type
 
 public class FixedPointNumber {
+    fun toInt() = 0
 }
+
+
--- type/src/test/kotlin/com/example/kn65op/android/lib/type/FixedPointNumberTest.kt
+++ type/src/test/kotlin/com/example/kn65op/android/lib/type/FixedPointNumberTest.kt
@@ -1,11 +1,15 @@
 package com.example.kn65op.android.lib.type;
 
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert
 import org.junit.Test
 
 class FixedPointNumberTest {
     @Test
-    fun dummy() {
-        assert(true)
+    fun NotInitializedDirectlyShouldBeEqualToZero() {
+        val number = FixedPointNumber()
+        val zero = 0
+        MatcherAssert.assertThat(number.toInt(), equalTo(zero))
     }
 
 }
