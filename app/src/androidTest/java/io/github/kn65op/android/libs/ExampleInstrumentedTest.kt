8dc5fa6871c4dd9fdd9b0f79c4367a6516eaf5df
Tomasz Drzewiecki <199537+kn65op@users.noreply.github.com>
2020-07-04 23:24:47 +0200

    Fix com.example and update gradle

--- app/src/androidTest/java/io/github/kn65op/android/libs/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/github/kn65op/android/libs/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.kn65op.android.libs", appContext.packageName)
+        assertEquals("io.github.kn65op.android.libs", appContext.packageName)
     }
 }
